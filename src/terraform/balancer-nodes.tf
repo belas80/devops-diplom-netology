e983f5a6f74e693c5f07268e79d4cc7527d342ac
Aleksandr Belyaev <belas80@gmail.com>
2022-12-18 14:04:26 +0500

    added prometheus monitoring manifests

--- src/terraform/balancer-nodes.tf
+++ src/terraform/balancer-nodes.tf
@@ -10,6 +10,15 @@ resource "yandex_lb_network_load_balancer" "nodes-lb" {
     }
   }
 
+  listener {
+    name        = "grafana"
+    port        = 3000
+    target_port = 30902
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
   attached_target_group {
     target_group_id = yandex_compute_instance_group.ig-nodes.load_balancer[0].target_group_id
 
