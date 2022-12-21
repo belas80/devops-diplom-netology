930280ee48ff9abfec3bc7d6925ee620ab22904e
Aleksandr Belyaev <belas80@gmail.com>
2022-12-22 01:30:43 +0500

    antlantis.yaml

---
+++ gen_atlantis_project.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+grep -P 'backend[\s]+"s3"' **/*.tf |
+  rev | cut -d'/' -f2- | rev |
+  sort |
+  uniq |
+  while read d; do \
+    echo '[ {"name": "'"$d"'","dir": "'"$d"'", "autoplan": {"when_modified": ["**/*.tf.*"] }} ]' | yq -PM; \
+  done
