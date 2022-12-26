a6788f9400a176c4c0396080d0bbc818e949cb8a
Aleksandr Belyaev <belas80@gmail.com>
2022-12-26 17:52:36 +0500

    proxy in ssh command

--- terraform/get_k8s_config.sh
+++ terraform/get_k8s_config.sh
@@ -6,7 +6,7 @@ bastion_ip=$(terraform output -json bastion_external_ip | jq -jc ".[0]")
 cp1_ip=$(terraform output -json cp_ips | jq -jc ".[][0]")
 lb_ip=$(terraform output -json lb_cp_external_ip | jq -jc ".[][0][0]")
 
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -J ubuntu@$bastion_ip ubuntu@$cp1_ip "sudo cat /etc/kubernetes/admin.conf" > ~/.kube/config
+ssh -o ProxyCommand="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %h:%p ubuntu@$bastion_ip" ubuntu@$cp1_ip "sudo cat /etc/kubernetes/admin.conf" > ~/.kube/config
 
 sed -i -- "s/127.0.0.1/$lb_ip/g" ~/.kube/config
 
